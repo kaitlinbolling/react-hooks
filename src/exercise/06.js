f0fe421bb41a0e97f149b3ca758650573a6ccd75
Kaitlin Bolling <kaitlin@alley.co>
2021-08-25 22:06:49 -0400

    exercise 06 extra 1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,6 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {
   PokemonForm,
   fetchPokemon,
@@ -16,6 +12,7 @@ import {
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
@@ -25,17 +22,23 @@ function PokemonInfo({pokemonName}) {
     }
 
     setPokemon(null);
-
+    setError(null);
     fetchPokemon(pokemonName).then(
       pokemonData => {
         setPokemon(pokemonData);
       },
-    );
+    ).catch((error) => setError(error));
 
   }, [pokemonName]);
 
-  if (!pokemonName) {
-    return 'Submit a pokemon'
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    );
+  } else if (!pokemonName) {
+    return 'Submit a pokemon';
   } else if (!pokemon) {
     return <PokemonInfoFallback name={pokemonName} />
   } else {
